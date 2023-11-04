707920e63f19685a94fed2cc18479b1aa5987808
sumi_raju <everlasting79uni@gmail.com>
2023-11-04 15:01:11 +0530

    first commit

---
+++ vector.py
@@ -0,0 +1,33 @@
+from IPython.display import Audio
+from scipy.io import wavfile
+import numpy as np
+
+file_name = 'harvard.wav'
+
+Audio(file_name)
+data = wavfile.read(file_name)
+framerate = data[0]
+sounddata = data[1]
+time = np.arange(0,len(sounddata))/framerate
+print('Sample rate:',framerate,'Hz')
+print('Total time:',len(sounddata)/framerate,'s')
+import soundfile as sf
+import librosa
+import torch
+from transformers import Wav2Vec2ForCTC, Wav2Vec2Tokenizer
+
+tokenizer = Wav2Vec2Tokenizer.from_pretrained("facebook/wav2vec2-base-960h")
+model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
+
+input_audio, _ = librosa.load(file_name,
+                              sr=16000)
+
+input_values = tokenizer(input_audio, return_tensors="pt").input_values
+logits = model(input_values).logits
+predicted_ids = torch.argmax(logits, dim=-1)
+transcription = tokenizer.batch_decode(predicted_ids)[0]
+
+print(transcription)
+
+
+
